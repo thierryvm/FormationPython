331ae28b65b06b0ac0ce443cabbc92a1bfab951e
thierryvm <46031203+thierryvm@users.noreply.github.com>
2019-01-03 17:54:45 +0100

    Activation du code teste

--- func.py
+++ func.py
@@ -1,11 +1,9 @@
-# def parler(personnage, message):
-#     print(f"{personnage} : {message}")
-#
-# def au_revoir():
-#     print("Au revoir :) !")
-#
-# if __name__ == "__main__":
-#     parler("Jason", "Bonjour tous le monde !")
-#     au_revoir()
+def parler(personnage, message):
+    print(f"{personnage} : {message}")
 
-import Jason
+def au_revoir():
+    print("Au revoir :) !")
+
+if __name__ == "__main__":
+    parler("Jason", "Bonjour tous le monde !")
+    au_revoir()
