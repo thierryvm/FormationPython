6530d35b2d59bf863d18bbbf18aee6005c7491c9
thierryvm <46031203+thierryvm@users.noreply.github.com>
2019-01-03 17:36:41 +0100

    Initial commit

---
+++ func.py
@@ -0,0 +1,11 @@
+# def parler(personnage, message):
+#     print(f"{personnage} : {message}")
+#
+# def au_revoir():
+#     print("Au revoir :) !")
+#
+# if __name__ == "__main__":
+#     parler("Jason", "Bonjour tous le monde !")
+#     au_revoir()
+
+import Jason
